a490a0fda03703a44643f7947f0067156d2cf0e7
Hovark25 <83977751+Hovark25@users.noreply.github.com>
2025-03-20 16:13:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("catsContainer");
+
+    cats.forEach(cat => {
+        const card = document.createElement("div");
+        card.classList.add("cat-card");
+
+        // Отмечаем любимцев красной рамкой
+        if (cat.favourite) {
+            card.classList.add("favourite");
+        }
+
+        card.innerHTML = `
+            <img src="${cat.img_link}" alt="${cat.name}">
+            <h2>${cat.name}</h2>
+            <p><strong>Возраст:</strong> ${cat.age} лет</p>
+            <p><strong>Рейтинг:</strong> ${cat.rate}/10</p>
+            <p>${cat.description}</p>
+        `;
+
+        container.appendChild(card);
+    });
+});
